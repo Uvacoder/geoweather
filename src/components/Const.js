a168ee53f4c7d6ef4a25329d77026bfac0ff06da
uvacoder <uva22940coder@gmail.com>
2023-02-21 16:30:54 -0500

    Update Const.js

--- src/components/Const.js
+++ src/components/Const.js
@@ -120,16 +120,8 @@ export const geolocationOptions = {
   maximumAge: 1000 * 3600 * 24 // 24 hour
 }
 export const DEFAULT_LOCATIONS = [
-  { latitude: 40.7128, longitude: -74.006 },
-  { latitude: 51.5074, longitude: -0.1278 },
-  { latitude: 48.8566, longitude: 2.3522 },
-  { latitude: 35.6895, longitude: 139.6917 },
-  { latitude: 55.7558, longitude: 37.6173 },
-  { latitude: 52.52, longitude: 13.405 },
-  { latitude: 37.7749, longitude: -122.4194 },
-  { latitude: 41.8781, longitude: -87.6298 },
-  { latitude: 39.9042, longitude: 116.4074 },
-  { latitude: 23.1291, longitude: 113.2644 },
+  { latitude: 38.031052, longitude: -78.474159 }
+ 
   { latitude: 28.6139, longitude: 77.209 },
   { latitude: 19.076, longitude: 72.8777 }
 ]
